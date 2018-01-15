9b5dc7b77940370e17d05c86202946bf24e964da
Jesus <Jesus@>
2018-01-15 14:43:24 +0100

    ver 0.2 (los formularios se conectan entre si, no hay base de datos)

---
+++ ProjecteM15Part1MVVMv0.2/ProjecteM15Part1MVVMv0.1/View/VistaCliente.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjecteM15Part1MVVMv0._1.View
+{
+    /// <summary>
+    /// Interaction logic for VistaAdmin.xaml
+    /// </summary>
+    public partial class VistaAdmin : Window
+    {
+        public VistaAdmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProjecteM15Part1MVVMv0.2/ProjecteM15Part1MVVMv0.1/ViewModel/VistaClientViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjecteM15Part1MVVMv0._1.ViewModel
+{
+    class VistaClientViewModel
+    {
+    }
+}
